6a0ae34730d8b3862785764d3b251c5a99c3396a
unknown <madaawadh1@gmail.com>
2024-09-15 20:41:52 +0300

    cat hw

---
+++ js.js
@@ -0,0 +1,23 @@
+const img=document.querySelector("img");
+const row=document.querySelector(".row");
+const resCodes = [
+    100, 101, 102, 103, 200, 201, 202, 203, 204, 206, 207, 208, 214, 226, 300,
+    301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407,
+    408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423,
+    424, 425, 426, 428, 429, 431, 444, 450, 451, 497, 498, 499, 500, 501, 502,
+    503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525, 530, 599,
+  ];
+function displayImg(){
+    resCodes.map((el)=>{
+        let im=document.createElement("img");
+       im.classList.add("col-md-4","col-sm-12","pb-4");
+    im.src=`https://http.cat/${el}.jpg`;
+    row.append(im);
+        
+    });
+    // let im=document.createElement("img");
+    // im.src=json.file;
+    // body.append(im);
+}
+
+displayImg();
